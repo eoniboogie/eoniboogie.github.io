910a8a334dc664449e4e044ee21403610adce1e3
eoniboogie <monkey9327@gmail.com>
2026-02-21 23:51:04 +0900

    first commit

---
+++ content/posts/first-post.md
@@ -0,0 +1,7 @@
++++
+date = '2026-02-21T23:34:58+09:00'
+draft = false
+title = 'First Post'
++++
+
+Hugo test!!
---
+++ public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "My New Hugo Site \u003e Posts",
+    "content": "Hugo test!!",
+    "description": "Hugo test!!",
+    "tags": [],
+    "title": "First Post",
+    "uri": "/posts/first-post/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "My New Hugo Site",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
